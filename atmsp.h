bd36d8c231c539235338038ecb578d081b2021a3
Dipakyadav-19 <84558329+Dipakyadav-19@users.noreply.github.com>
2022-08-05 09:39:39 +0530

    Update atmsp.h

--- atmsp.h
+++ atmsp.h
@@ -1,17 +1,4 @@
-/** *********************************************************************** **
- ** Copyright (C) 1989-2013 Heinz van Saanen                                **
- **                                                                         **
- ** This file may be used under the terms of the GNU General Public         **
- ** License version 3 or later as published by the Free Software Foundation **
- ** and appearing in the file LICENSE.GPL included in the packaging of      **
- ** this file.                                                              **
- **                                                                         **
- ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE **
- ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR        **
- ** PURPOSE.                                                                **
- **                                                                         **
- ** Verson: 1.0.4                                                           **
- ** *********************************************************************** **/
+
 
 #ifndef _ATMSP_H_INCLUDED_
 #define _ATMSP_H_INCLUDED_
